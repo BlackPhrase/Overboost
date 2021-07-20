acdd1852f3e34c5c1474b28f53a5f2747d7a197f
BlackPhrase <BlackPhrase@users.noreply.github.com>
2021-07-20 11:26:18 +0000

    Include qshared header inside the qcommon

--- code/common/qcommon/qcommon.h
+++ code/common/qcommon/qcommon.h
@@ -23,6 +23,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #ifndef _QCOMMON_H_
 #define _QCOMMON_H_
 
+#include "qshared/q_shared.h"
 #include "cm_public.h"
 
 //#define	PRE_RELEASE_DEMO
