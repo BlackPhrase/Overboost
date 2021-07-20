78839e1f38f0af7783d4472f555274d0bd8f8cab
BlackPhrase <BlackPhrase@users.noreply.github.com>
2021-07-20 00:21:10 +0000

    Update the engine initialization and exports code

--- code/engine/Engine.cpp
+++ code/engine/Engine.cpp
@@ -2,6 +2,7 @@
 
 #include "Engine.hpp"
 #include "engine/IEngine.h"
+#include "qcommon/qcommon.h"
 
 #ifdef _WIN32
 #	include "qcommon/win/win_local.h"
@@ -16,41 +17,63 @@
 namespace
 {
 
-bool Engine_Init(const InitProps &aInitProps)
+bool Engine_Init(const engine_export_t::InitProps &aInitProps)
 {
-	char		cwd[MAX_OSPATH];
-	int			startTime, endTime;
+#ifdef _WIN32
+	g_wv.hInstance = hInstance;
+#endif
 
 	// done before Com/Sys_Init since we need this for error output
 	Sys_CreateConsole();
 
+#ifdef _WIN32
 	// no abort/retry/fail errors
 	SetErrorMode( SEM_FAILCRITICALERRORS );
+#endif
 
 	// get the initial time base
 	Sys_Milliseconds();
+	
 #if 0
 	// if we find the CD, add a +set cddir xxx command line
 	Sys_ScanForCD();
 #endif
 
 	Sys_InitStreamThread();
-	
+
 	Com_Init(aInitProps.sCmdLine); // sys_cmdline
 	NET_Init();
 
+#ifdef _WIN32
+	char cwd[MAX_OSPATH];
 	_getcwd (cwd, sizeof(cwd));
 	Com_Printf("Working directory: %s\n", cwd);
+#endif
 
 	// hide the early console since we've reached the point where we
 	// have a working graphics subsystems
 	if ( !com_dedicated->integer && !com_viewlog->integer )
 		Sys_ShowConsole( 0, qfalse );
+	
+#ifdef __UNIX__
+	Sys_ConsoleInputInit();
+
+	fcntl(0, F_SETFL, fcntl (0, F_GETFL, 0) | FNDELAY);
+	
+#	ifdef DEDICATED
+		// init here for dedicated, as we don't have GLimp_Init
+		InitSig();
+#	endif
+#endif
+	
+	return true;
 };
 
 void Engine_Frame()
 {
+#ifdef _WIN32
 	static int totalMsec, countMsec;
+	int startTime, endTime;
 	
 	// if not running as a game client, sleep a bit
 	if ( g_wv.isMinimized || ( com_dedicated && com_dedicated->integer ) )
@@ -66,19 +89,26 @@ void Engine_Frame()
 
 	// make sure mouse and joystick are only called once a frame
 	IN_Frame();
+#endif
+
+#ifdef __linux__
+    Sys_ConfigureFPU();
+#endif
 
 	// run the game
 	Com_Frame();
 
+#ifdef _WIN32
 	endTime = Sys_Milliseconds();
 	totalMsec += endTime - startTime;
 	countMsec++;
+#endif
 };
 
 engine_export_t gEngine =
 {
 	Engine_Init,
-	Com_Shudown,
+	Com_Shutdown,
 	Engine_Frame
 };
 
@@ -87,7 +117,7 @@ engine_export_t gEngine =
 C_EXPORT engine_export_t *GetEngineAPI(int nAPIVersion)
 {
 	if(nAPIVersion == ENGINE_API_VERSION)
-		return gEngine;
+		return &gEngine;
 	
 	return nullptr;
 };
